4f50f6dda2b84181c82e0e2159be80c3f1b74d63
Mnigos <makowskiigor@gmail.com>
2021-10-01 11:30:17 +0200

    refactor(FormElement): add CSSTransition

--- components/FormElement.tsx
+++ components/FormElement.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable unicorn/no-nested-ternary */
 import { ChangeEvent, SelectHTMLAttributes } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 type OnChange = SelectHTMLAttributes<
   HTMLSelectElement | HTMLInputElement
@@ -40,92 +41,86 @@ export default function FormElement({
     if (value && checkOnChange) checkOnChange(value as string[])
   }
 
-  if (!show)
-    return (
-      <div className="flex justify-start h-16 mt-2 w-96">
-        <div className="flex flex-col items-center">
-          <div className="flex items-center justify-center w-10 h-10 text-2xl font-semibold bg-gray-300 rounded-full">
-            {stepNumber}
-          </div>
-          <div className="w-0.5 h-1/6 mt-3 bg-gray-300"></div>
-        </div>
-
-        <div className="flex flex-col justify-between mt-1 ml-8">
-          <label className="text-xl font-semibold">{title}</label>
-        </div>
-      </div>
-    )
-
   return (
-    <div className="flex justify-start h-40 mt-2 w-96">
+    <div
+      className={`flex justify-start ${
+        show ? 'h-40' : 'h-16'
+      } mt-2 w-96 transition-all duration-300 ease-in-out`}
+    >
       <div className="flex flex-col items-center">
         <div className="flex items-center justify-center w-10 h-10 text-2xl font-semibold bg-gray-300 rounded-full">
           {stepNumber}
         </div>
-        <div className="w-0.5 h-2/3 mt-3 bg-gray-300"></div>
+        <div
+          className={`w-0.5 ${show ? 'h-2/3' : 'h-1/6'} mt-3 bg-gray-300`}
+        ></div>
       </div>
 
       <div className="flex flex-col justify-between h-full mt-1 ml-8">
         <label className="text-xl font-semibold">{title}</label>
 
-        {options && !checkbox ? (
-          <select name={name} value={value} onChange={onChange as OnChange}>
-            {options?.map((option, index) => (
-              <option value={option} key={index} className="cursor-pointer">
-                {option}
-              </option>
-            ))}
-          </select>
-        ) : checkbox ? (
+        <CSSTransition in={show} timeout={150} classNames="form" unmountOnExit>
           <>
-            {options?.map((option, index) => (
-              <span key={index} className="flex items-center">
-                <input
-                  type="checkbox"
-                  className="w-6 h-4"
-                  value={option}
-                  onChange={handleCheckBoxChange}
-                />
-                <label>{option}</label>
-              </span>
-            ))}
-          </>
-        ) : (
-          <input
-            value={value}
-            name={name}
-            onChange={onChange}
-            placeholder={title}
-          />
-        )}
+            {options && !checkbox ? (
+              <select name={name} value={value} onChange={onChange as OnChange}>
+                {options?.map((option, index) => (
+                  <option value={option} key={index} className="cursor-pointer">
+                    {option}
+                  </option>
+                ))}
+              </select>
+            ) : checkbox ? (
+              <>
+                {options?.map((option, index) => (
+                  <span key={index} className="flex items-center">
+                    <input
+                      type="checkbox"
+                      className="w-6 h-4"
+                      value={option}
+                      onChange={handleCheckBoxChange}
+                    />
+                    <label>{option}</label>
+                  </span>
+                ))}
+              </>
+            ) : (
+              <input
+                value={value}
+                name={name}
+                onChange={onChange}
+                placeholder={title}
+              />
+            )}
 
-        <div>
-          {!!goToBackStep && (
-            <button className="mr-4 btn" onClick={goToBackStep}>
-              Back
-            </button>
-          )}
-          <button
-            className={
-              checkbox
-                ? value?.toString() === options?.toString()
-                  ? 'btn'
-                  : 'btn-disabled cursor-not-allowed'
-                : value
-                ? 'btn'
-                : 'btn-disabled cursor-not-allowed'
-            }
-            disabled={
-              checkbox
-                ? (value as string[])?.sort().toString() !==
-                  options?.sort().toString()
-                : !value
-            }
-            onClick={goToNextStep}
-          >
-            Next
-          </button>
-        </div>
+            <div>
+              {!!goToBackStep && (
+                <button className="mr-4 btn" onClick={goToBackStep}>
+                  Back
+                </button>
+              )}
+              <button
+                className={
+                  checkbox
+                    ? value?.toString() === options?.toString()
+                      ? 'btn'
+                      : 'btn-disabled cursor-not-allowed'
+                    : value
+                    ? 'btn'
+                    : 'btn-disabled cursor-not-allowed'
+                }
+                disabled={
+                  checkbox
+                    ? (value as string[])?.sort().toString() !==
+                      options?.sort().toString()
+                    : !value
+                }
+                onClick={goToNextStep}
+              >
+                Next
+              </button>
+            </div>
+          </>
+        </CSSTransition>
       </div>
     </div>
   )
