41a7481a87a811072eb8822e06746b9a5975f771
Mnigos <makowskiigor@gmail.com>
2021-10-06 16:57:08 +0200

    chore(config): create jest configuration files

---
+++ jest.config.mjs
@@ -0,0 +1,32 @@
+const Config = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  modulePaths: ['node_modules', 'src'],
+  roots: ['src'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
+}
+
+export default Config
